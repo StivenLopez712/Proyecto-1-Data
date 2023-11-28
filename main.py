2aa0dd752d063d8cf6049518099c29581c221934
StivenLopez712 <jstivenlopezm@gmail.com>
2023-11-27 23:16:26 -0500

    actualizacion main.py

--- main.py
+++ main.py
@@ -1,26 +1,38 @@
 from fastapi import FastAPI
-from fastapi.responses import JSONResponse
-from typing import List
-
 import pandas as pd
-#from sklearn.metrics.pairwise import cosine_similarity
-#from sklearn.preprocessing import MinMaxScaler
-
+import numpy as np
 app = FastAPI()
 
-DF_GAMES = pd.read_parquet('Parquets/ETL_Steam_Games.gzip')
-DF_REVIEWS = pd.read_parquet('Parquets/Review_Sentiment_Analysis.gzip')
-DF_ITEMS = pd.read_parquet('Parquets/ETL_user_items.gzip')
-recomendaciones = pd.read_parquet('Parquets/Recomendacion.gzip')
+# Cargar los datos necesarios
+tabla_funciones = pd.read_parquet('Tabla_API.parquet')
+F3 = pd.read_parquet('F3.parquet')
+F4 = pd.read_parquet('F4.parquet')
+F5 = pd.read_parquet('F5.parquet')
+matrix_norm = pd.read_parquet('matrix_norm.parquet')
+item_sim_df = pd.read_parquet('item_sim_df.parquet')
 
 # Función 1
-@app.get('/PlayTimeGenre/{genero}')
+@app.get('/PlayTimeGenre')
 def play_time_genre(genero: str):
-    merged_df = pd.merge(DF_GAMES, DF_ITEMS, on='Item_Id')
-    genre_df = merged_df[merged_df['Genres'] == genero]
-    grouped_df = genre_df.groupby('Release_Year')['Playtime_Forever'].sum().reset_index()
-    max_year = grouped_df.loc[grouped_df['Playtime_Forever'].idxmax(), 'Release_Year']
+    max_year = tabla_funciones[tabla_funciones['Genres'].str.contains(genero, case=False, na=False)]\
+        .groupby('Release_Year')['Playtime_Forever'].sum().idxmax()
     result = {"Año de lanzamiento con más horas jugadas para Género " + genero: int(max_year)}
     return result
+# Función 5
+@app.get('/sentiment_analysis')
+def sentiment_analysis(desarrolladora: str):
+    sentiment_counts = F5['Sentiment_Analysis'].value_counts().to_dict()
+    output_dict = {desarrolladora: {'Negative': sentiment_counts.get(0, 0),
+                                    'Neutral': sentiment_counts.get(1, 0),
+                                    'Positive': sentiment_counts.get(2, 0)}}
+    return output_dict
 
-
+# Función 6
+@app.get('/recomendacion_juego')
+def recomendacion_juego(id_producto):
+    count = 1
+    result = {'mensaje': 'Similar games include:', 'juegos_recomendados': []}
+    for item in item_sim_df.sort_values(by=id_producto, ascending=False).index[1:6]:
+        result['juegos_recomendados'].append(''.join(item))
+        count += 1
+    return result
